44949d61f56bb621864a1a340317a5fed4a18098
Yasin047 <95363870+Yasin047@users.noreply.github.com>
2023-08-06 21:38:34 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Routes and Route components
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"; // Import Routes and Route components
 import { Dapp } from "./components";
-import Index from './components/Homev';
+import Index from "./components/Homev";
 const App = () => (
   <Router>
     {/* Wrap your Route components in a Routes component */}
     <Routes>
       {/* Add a Route for the Dapp component */}
-      <Route exact path="/" element={<Index />} />
-      <Route exact path="/dapp" element={<Dapp />} />
+      <Route path="/" element={<Index />} />
+      <Route path="/dapp" element={<Dapp />} />
     </Routes>
   </Router>
 );
